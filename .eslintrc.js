782d92b1940cdb722eaa1a074b74ae3a032d6085
Michael Puzon <michaelp@moback.com>
2017-04-11 10:06:37 -0700

    add linting

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+     "react",
+     "jsx-a11y",
+     "import"
+  ],
+  "rules": {
+    "linebreak-style": 0,
+    "comma-dangle": 0,
+    "max-len": [1, 120, 2, {ignoreComments: true}],
+  },
+};
